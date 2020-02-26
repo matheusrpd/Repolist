e77feba9f997f7805044f75626acbbd59b4dcfb8
Matheus Allan <matheusrpd@gmail.com>
2020-02-26 15:24:22 -0300

    Finalizado page do repostiorio, mostrando suas issues.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default function Repository({ match }) {
+  const [repository, setRepository] = useState();
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadRepository = async () => {
+      const repoName = decodeURIComponent(match.params.repository);
+
+      const [responseRepo, responseIssue] = await Promise.all([
+        await api.get(`/repos/${repoName}`),
+        await api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_parge: 5,
+          },
+        }),
+      ]);
+
+      setRepository(responseRepo.data);
+      setIssues(responseIssue.data);
+      setLoading(false);
+    };
+
+    loadRepository();
+  }, []);
+
+  return (
+    <>
+      {loading && <Loading>Carregando...</Loading>}
+      {!loading && (
+        <Container>
+          <Owner>
+            <Link to="/">Voltar aos repositórios</Link>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <h1>{repository.name}</h1>
+            <p>{repository.description}</p>
+          </Owner>
+
+          <IssueList>
+            {issues.map(issue => (
+              <li key={String(issues.id)}>
+                <img src={issue.user.avatar_url} alt={issue.user.login} />
+                <div>
+                  <strong>
+                    <a href={issue.html_url}>{issue.title}</a>
+                    {issue.labels.map(label => (
+                      <span key={String(label.id)}>{label.name}</span>
+                    ))}
+                  </strong>
+                  <p>{issue.user.login}</p>
+                </div>
+              </li>
+            ))}
+          </IssueList>
+        </Container>
+      )}
+    </>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.shape(),
+    }),
+  }).isRequired,
+};
